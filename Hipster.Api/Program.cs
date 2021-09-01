64fe901d3711448ccbfdec45fad9548da7431bc6
Boris Panasyuk <bpanasyuk@playtika.com>
2021-09-01 19:03:34 +0300

    Heroku integration

--- Hipster.Api/Program.cs
+++ Hipster.Api/Program.cs
@@ -20,7 +20,7 @@ namespace Hipster.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseHeroku();
                 });
     }
 }
